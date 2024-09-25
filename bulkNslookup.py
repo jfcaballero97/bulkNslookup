5992093c10130899f293c22250dac37f983cf76b
Juan Fco Caballero Herrera <43372669+jfcaballero97@users.noreply.github.com>
2024-09-25 16:10:49 +0200

    Update bulkNslookup.py
    After every consult, the result is checked to find if there is any public IP among the response

--- bulkNslookup.py
+++ bulkNslookup.py
@@ -16,12 +16,36 @@ os.system('cls')
 print('Results:\r\n')
 
 for i in fqdnList:
-    print('- ' + i)    
+    responseIPs = [] 
+    print('\r\n- ' + i)    
     result = subprocess.run('nslookup ' + i, shell=True, capture_output=True, text=True)
     cmdResponse = result.stdout
     cmdError = result.stderr
+
     if int(cmdResponse.find('Name')) == -1:
         print('No FQDN found\r\n')
-    else: print(cmdResponse[cmdResponse.find('Name'):])
+    else:
+         cmdResponse = cmdResponse[cmdResponse.find('Name'):]
+         print(cmdResponse)
+         if cmdResponse.find('Address: ')+len('Address: ') != -1:
+              aux = cmdResponse[cmdResponse.find('Address: ')+len('Address: '):]
+              aux = re.split(r'[^0-9.]+', aux)
+         else:
+            aux = cmdResponse[cmdResponse.find('Addresses: ')+len('Addresses: '):]
+            aux = re.split(r'[^0-9.]+', aux)
+         for i in aux:
+             try:
+                 ipaddress.ip_address(i)
+                 responseIPs.append(i)
+             except ValueError:
+                 error = i + ' is not a valid IP'
+		
+         isPublic = False
+         for ip in responseIPs:
+              if ipaddress.IPv4Address(ip).is_private == False:
+                   isPublic = True
+         if isPublic: print('Public IPs have been found in the FQDN.')
+		
+         else: print('Everything is private ;)')
 
 os.system('pause')
